573012569fe9a70560b580e7ee5e6c3f32036e9f
dozhou <dozhou@rubiconproject.com>
2019-01-06 02:29:14 -0800

    add table at stack implement

--- src/stack/animator.ts
+++ src/stack/animator.ts
@@ -2,16 +2,31 @@ import { IStack } from "./stack";
 
 export default class <T> implements IStack<T> {
 
+    private table: HTMLTableElement;
+
+    constructor(public speed?: number) {
+        this.table = document.createElement("table");
+    }
+
     push(item: T): T {
-        throw new Error("Method not implemented.");
+        const row: HTMLTableRowElement = this.table.insertRow(0);
+        const cell: HTMLTableCellElement = row.insertCell(0);
+        cell.innerHTML = item + "";
+        return item;
     }
 
     peek(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.table.firstChild) {
+            // TODO update styles 
+        }
+        return undefined;
     }
 
     pop(): T | undefined {
-        throw new Error("Method not implemented.");
+        if (this.table.firstChild) {
+            this.table.deleteRow(0);
+        }
+        return undefined;
     }
 
     size(): number {
@@ -20,5 +35,9 @@ export default class <T> implements IStack<T> {
 
     isEmpty(): boolean {
         throw new Error("Method not implemented.");
+    }
+
+    get element() {
+        return this.table;
     }
 }
